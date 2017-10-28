9a0c641091352f7af5574d1afe774a7b8972cf89
Sahil Satishkumar <sahilsatishkumar@gmail.com>
2017-10-28 17:14:12 +0530

    chore(configured): semantic ui

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Container, Grid, Menu, Segment } from 'semantic-ui-react'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      activeItem: 'bio'
+    }
+  }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
   render() {
+    const { activeItem } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Grid>
+        <Grid.Column stretched width={12}>
+          <Segment>
+            This is an stretched grid column. This segment will always match the tab height
+            </Segment>
+        </Grid.Column>
+
+        <Grid.Column width={4}>
+          <Menu fluid vertical tabular='right'>
+            <Menu.Item name='bio' active={activeItem === 'bio'} onClick={this.handleItemClick} />
+            <Menu.Item name='pics' active={activeItem === 'pics'} onClick={this.handleItemClick} />
+            <Menu.Item name='companies' active={activeItem === 'companies'} onClick={this.handleItemClick} />
+            <Menu.Item name='links' active={activeItem === 'links'} onClick={this.handleItemClick} />
+          </Menu>
+        </Grid.Column>
+      </Grid>
+    )
   }
 }
 
